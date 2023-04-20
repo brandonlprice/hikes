a3449dd66ad83276d29f9799dec81fa764d3e9dc
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2023-04-20 16:35:18 -0700

    Add files via upload

---
+++ carbon-cyn/demo.js
@@ -0,0 +1,193 @@
+let demojs={
+	"storymap": {
+		"calculate_zoom": false,
+		"attribution": "",
+		"call_to_action": false,
+		"show_history_line":false,
+		"zoomify": false,
+		"map_type": "http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}",
+		"call_to_action_text": "",
+		"map_as_image": false,
+		"map_subdomains":"xyz",
+		"slides": [
+			{
+			"date": "Jan 2006",
+			"media": {
+				"url": "images/topoftheworld.JPG",
+				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
+				"credit": ""
+			},
+			"type": "overview",
+			"location": {
+				"line": false
+			},
+			"text": {
+				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>West Ridge Trail to the Top of the World</h4>",
+				"text": "This trail takes you to one of the most visited scenic destinations in Orange County called \"Top of the World\". It's known for being one of the highest elevation points with such a close proximity to the ocean in the county. It stands slightly above 1,000 feet. And even though it's not as tall as the 2,000 plus feet Santa Monica Mountains right off the Malibu Coast in LA County, it's still worth checking out. There are four main ways to get there: this way which starts in Aliso Viejo, the Canyon Acres Trail in Laguna Beach, and another unnamed trail in Laguna Beach, and driving there."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/parking.jpeg",
+				"caption": "Park on Hollyleaf",
+				"credit": ""
+			},
+			"location": { 
+				"lat":  33.58439031741074,
+				"line": false,
+				"lon": -117.74963056244559,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 miles</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Out & Back</li></ul></small>",
+				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/entrance.jpeg",
+				"caption": "",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.5847, 
+				"line": false,
+				"lon": -117.7497,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Starting Point</h6>",
+				"text": "Once parked, this is where you'll start. This isn't the official trail yet. Just an access route into Aliso & Wood Canyons Regional Park. It's all fairly steep and uphill, but a short distance."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/trailhead.jpeg",
+				"caption": "",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.58373173352325,
+				"line": false,
+				"lon": -117.74977338126365,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The West Ridge Trail</h6>",
+				"text": "Once at the top, you are now within the county park and at the trailhead for West Ridge. You'll stay on this same trail for the next 2 miles until reaching your destination. Be sure to stay on this trail and not accidentally take another park trail. This can happen since the trails have no signage at some junctions. However, since this trail is a ridge trail, if you inadvertently take the wrong trail, you would know because you would be declining off the ridge."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_4.JPG",
+				"caption": "<i>Views of Wood Canyon to the east.</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 	33.58210336055123,
+				"line": false,
+				"lon": -117.75212193690419,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Enjoy the Views</h6>",
+				"text": "Continue straight on this trail all the way down to Top of the World while enjoying the views along the way. You'll see the 133 on your right way down below and nice views of Wood Canyon on your left."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_5.JPG",
+				"caption": "<i></i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.577675362848936,
+				"line": false,
+				"lon": -117.754954824902,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Stay Straight</h6>",
+				"text": "Pretty straight forward pathway to the destination. Just watch out for one section that has several unmarked trail junctions. If you go the wrong way, you'll know right away as previously suggested because you would be declining from the ridge."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_6.JPG",
+				"caption": "<i>One out of of a handful of steep inclines.</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.5637,
+				"line": false,
+				"lon": -117.7575,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Steep Inclines</h6>",
+				"text": "As you get closer to the destination, be aware that you'll encounter very short-lived yet very steep inclines. These inclines do not seem as tough on your legs on the way back though. Mountain bikers often will be present on the trail using these inlines to practice going uphill. Take the uphill going right to stay on the West Ridge Trail rather than taking the hard left down Mathis Canyon."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_7.JPG",
+				"caption": "<i>Nice view looking back at all the ground covered.</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.5616,
+				"line": false,
+				"lon": -117.7593,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Nearing the End</h6>",
+				"text": "After the last uphill you should see the Top of the World in the distance. At this point it should be fairly flat the last quarter of a mile until getting to Top of the World. There is a nice view looking back which you can check, otherwise you'll see it later on going back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/topoftheworld.JPG",
+				"caption": "<i>Entering Top of the World</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.55627577298641,
+				"line": false,
+				"lon": -117.7603500807199,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Entering the Summit</h6>",
+				"text": "Keep going straight until the trail ends. You'll feel like you're leaving the park and entering back into city as you should see more people and a street in front of you. Rather than taking the street, loop around to the right for the short trek up to the Top of the World."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/topoftheworld2.JPG",
+				"caption": "<i>Top of the World, reaching the summit</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.557617567533086,
+				"line": false,
+				"lon": -117.7608647546779,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Top of the World</h6>",
+				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+			}
+			} 
+		]
+	}
+  }
