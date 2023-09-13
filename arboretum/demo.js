6db34549c09f03b4c2077f6e10604766d05c8308
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2023-09-12 20:35:44 -0700

    Update demo.js

--- arboretum/demo.js
+++ arboretum/demo.js
@@ -83,7 +83,79 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "images/3.jpg",
+				"url": "images/IMG_0641_11zon.jpg",
+				"caption": "",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.887618,
+				"line": false,
+				"lon": -117.884009,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The West Ridge Trail</h6>",
+				"text": "Once at the top, you are now within the county park and at the trailhead for West Ridge. You'll stay on this same trail for the next 2 miles until reaching your destination. Be sure to stay on this trail and not accidentally take another park trail. This can happen since the trails have no signage at some junctions. However, since this trail is a ridge trail, if you inadvertently take the wrong trail, you would know because you would be declining off the ridge."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_0642_11zon.jpg",
+				"caption": "",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.887618,
+				"line": false,
+				"lon": -117.884009,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The West Ridge Trail</h6>",
+				"text": "Once at the top, you are now within the county park and at the trailhead for West Ridge. You'll stay on this same trail for the next 2 miles until reaching your destination. Be sure to stay on this trail and not accidentally take another park trail. This can happen since the trails have no signage at some junctions. However, since this trail is a ridge trail, if you inadvertently take the wrong trail, you would know because you would be declining off the ridge."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_0644_11zon (1).jpg",
+				"caption": "",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.887618,
+				"line": false,
+				"lon": -117.884009,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The West Ridge Trail</h6>",
+				"text": "Once at the top, you are now within the county park and at the trailhead for West Ridge. You'll stay on this same trail for the next 2 miles until reaching your destination. Be sure to stay on this trail and not accidentally take another park trail. This can happen since the trails have no signage at some junctions. However, since this trail is a ridge trail, if you inadvertently take the wrong trail, you would know because you would be declining off the ridge."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_0645.jpg",
+				"caption": "",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.887618,
+				"line": false,
+				"lon": -117.884009,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The West Ridge Trail</h6>",
+				"text": "Once at the top, you are now within the county park and at the trailhead for West Ridge. You'll stay on this same trail for the next 2 miles until reaching your destination. Be sure to stay on this trail and not accidentally take another park trail. This can happen since the trails have no signage at some junctions. However, since this trail is a ridge trail, if you inadvertently take the wrong trail, you would know because you would be declining off the ridge."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/4.jpg",
 				"caption": "<i>Views of Wood Canyon to the east.</i>",
 				"credit": ""
 			},
@@ -101,7 +173,7 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "images/6.jpg",
+				"url": "images/IMG_0650.jpg",
 				"caption": "<i></i>",
 				"credit": ""
 			},
