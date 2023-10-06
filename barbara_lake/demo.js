d6fda1a809f5244979244522f8c633eedb0d911b
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2023-10-05 22:28:29 -0700

    Update demo.js

--- barbara_lake/demo.js
+++ barbara_lake/demo.js
@@ -29,7 +29,43 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "images/IMG_9870_11zon.jpg",
+				"url": "images/IMG_1101_11zon.jpg",
+				"caption": "Park on Hollyleaf",
+				"credit": ""
+			},
+			"location": { 
+				"lat":  33.599386, 
+				"line": false,
+				"lon": -117.747328,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>3 miles</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Out & Back</li></ul></small>",
+				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_1092_11zon.jpg",
+				"caption": "Park on Hollyleaf",
+				"credit": ""
+			},
+			"location": { 
+				"lat":  33.599386, 
+				"line": false,
+				"lon": -117.747328,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>3 miles</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Out & Back</li></ul></small>",
+				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_1099_11zon.jpg",
 				"caption": "Park on Hollyleaf",
 				"credit": ""
 			},
