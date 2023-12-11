73a618a3d5d51cb791baf36e3c942531312adac9
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2023-12-11 06:05:18 -0800

    Update demo.js

--- live-oak-nature-dr/demo.js
+++ live-oak-nature-dr/demo.js
@@ -371,7 +371,7 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "images/IMG_7297_11zon.jpg",
+				"url": "images/IMG_2164_11zon.jpg",
 				"caption": "<i>Top of the World, reaching the summit</i>",
 				"credit": ""
 			},
