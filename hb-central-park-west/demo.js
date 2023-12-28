00ad991ff7c0d71fc1bd21048e052faff87fb1b6
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2023-12-28 01:52:44 -0800

    Update demo.js

--- hb-central-park-west/demo.js
+++ hb-central-park-west/demo.js
@@ -335,6 +335,42 @@ let demojs={
 			{
 			"date": "",
 			"media": {
+				"url": "images/IMG_2274_11zon.jpg",
+				"caption": "<i>Nice view looking back at all the ground covered.</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.69780114827692,     
+				"line": false,
+				"lon": -118.01292553577724,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Nearing the End</h6>",
+				"text": "After the last uphill you should see the Top of the World in the distance. At this point it should be fairly flat the last quarter of a mile until getting to Top of the World. There is a nice view looking back which you can check, otherwise you'll see it later on going back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/IMG_2283_11zon.jpg",
+				"caption": "<i>Nice view looking back at all the ground covered.</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.69803783498806,     
+				"line": false,
+				"lon": -118.01327892992575,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Nearing the End</h6>",
+				"text": "After the last uphill you should see the Top of the World in the distance. At this point it should be fairly flat the last quarter of a mile until getting to Top of the World. There is a nice view looking back which you can check, otherwise you'll see it later on going back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
 				"url": "images/IMG_1267_11zon.jpg",
 				"caption": "<i>Nice view looking back at all the ground covered.</i>",
 				"credit": ""
