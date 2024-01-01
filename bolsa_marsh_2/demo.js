4d5ad5513b891cbce2997e0aa6ea7217662405f2
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-01-01 09:39:08 -0800

    Update demo.js

--- bolsa_marsh_2/demo.js
+++ bolsa_marsh_2/demo.js
@@ -47,6 +47,24 @@ let demojs={
 			{
 			"date": "",
 			"media": {
+				"url": "images/IMG_2329_11zon.jpg",
+				"caption": "Park on Hollyleaf",
+				"credit": ""
+			},
+			"location": { 
+				"lat":  33.71133668222209, 
+				"line": false,
+				"lon":  -118.0605841435743,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 miles</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Out & Back</li></ul></small>",
+				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
+			}
+			},
+			{
+			"date": "",
+			"media": {
 				"url": "images/IMG_4380_11zon.jpg",
 				"caption": "",
 				"credit": ""
