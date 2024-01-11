92e9f4cc82d90156f9c58c50e3df214ae89eb890
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-01-11 05:10:30 -0800

    Update demo.js

--- aliso_summit/demo.js
+++ aliso_summit/demo.js
@@ -173,6 +173,24 @@ let demojs={
 			{
 			"date": "",
 			"media": {
+				"url": "images/AAE66807-562F-415D-A22D-CD6A52D74EC3_11zon.jpeg",
+				"caption": "<i>One out of of a handful of steep inclines.</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.523072942851925,  
+				"line": false,
+				"lon": -117.72779545487919,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Steep Inclines</h6>",
+				"text": "As you get closer to the destination, be aware that you'll encounter very short-lived yet very steep inclines. These inclines do not seem as tough on your legs on the way back though. Mountain bikers often will be present on the trail using these inlines to practice going uphill. Take the uphill going right to stay on the West Ridge Trail rather than taking the hard left down Mathis Canyon."
+			}
+			},
+			{
+			"date": "",
+			"media": {
 				"url": "images/4.jpg",
 				"caption": "<i>Nice view looking back at all the ground covered.</i>",
 				"credit": ""
