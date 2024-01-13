fe06709a0df9d6264818319873156b0c9bac62a4
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-01-13 08:19:26 -0800

    Update demo.js

--- billygoat-stairmasters/demo.js
+++ billygoat-stairmasters/demo.js
@@ -101,24 +101,6 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "../redrock-cyn/images/IMG_7995_11zon.jpg",
-				"caption": "<i></i>",
-				"credit": ""
-			},
-			"location": {
-				"lat": 33.577675362848936,
-				"line": false,
-				"lon": -117.754954824902,
-				"zoom": 19
-			},
-			"text": {
-				"headline": "<h6 style='color:red;'>Stay Straight</h6>",
-				"text": "Pretty straight forward pathway to the destination. Just watch out for one section that has several unmarked trail junctions. If you go the wrong way, you'll know right away as previously suggested because you would be declining from the ridge."
-			}
-			},
-			{
-			"date": "",
-			"media": {
 				"url": "../redrock-cyn/images/IMG_7998_11zon.jpg",
 				"caption": "<i>One out of of a handful of steep inclines.</i>",
 				"credit": ""
@@ -132,6 +114,24 @@ let demojs={
 			"text": {
 				"headline": "<h6 style='color:red;'>Steep Inclines</h6>",
 				"text": "As you get closer to the destination, be aware that you'll encounter very short-lived yet very steep inclines. These inclines do not seem as tough on your legs on the way back though. Mountain bikers often will be present on the trail using these inlines to practice going uphill. Take the uphill going right to stay on the West Ridge Trail rather than taking the hard left down Mathis Canyon."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "../redrock-cyn/images/IMG_7995_11zon.jpg",
+				"caption": "<i></i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.577675362848936,
+				"line": false,
+				"lon": -117.754954824902,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Stay Straight</h6>",
+				"text": "Pretty straight forward pathway to the destination. Just watch out for one section that has several unmarked trail junctions. If you go the wrong way, you'll know right away as previously suggested because you would be declining from the ridge."
 			}
 			},
 			{
