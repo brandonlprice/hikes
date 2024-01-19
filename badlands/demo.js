1caa57e5a95aedffe48ab614003a9176c21c8b6e
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-01-19 07:19:33 -0800

    Update demo.js

--- badlands/demo.js
+++ badlands/demo.js
@@ -227,6 +227,24 @@ let demojs={
 			{
 			"date": "",
 			"media": {
+				"url": "images/B5674BF1-5CD2-4470-9893-79B3A66F2B61_11zon.jpeg",
+				"caption": "<i>Top of the World, reaching the summit</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.49819981959302, 
+				"line": false,
+				"lon": 	-117.733033536801,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Top of the World</h6>",
+				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
 				"url": "images/IMG_9396_11zon.jpg",
 				"caption": "<i>Top of the World, reaching the summit</i>",
 				"credit": ""
