634605cd8c184240c5281017675238e141edbe2b
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-01 04:34:54 -0800

    Update demo.js

--- serrano-ridge/demo.js
+++ serrano-ridge/demo.js
@@ -245,6 +245,60 @@ let demojs={
 			{
 			"date": "",
 			"media": {
+				"url": "images/8D1D9DD4-E160-4B50-A1E8-72150FFD2668_11zon.jpeg",
+				"caption": "<i>Top of the World, reaching the summit</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.6100205183133, 
+				"line": false,
+				"lon": -117.77434009030009,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Top of the World</h6>",
+				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/E19DEA2E-5BCE-463B-8745-C15FDDB83ACE_11zon.jpeg",
+				"caption": "<i>Top of the World, reaching the summit</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.60750618092324,  
+				"line": false,
+				"lon": -117.77496918741264,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Top of the World</h6>",
+				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
+				"url": "images/E19DEA2E-5BCE-463B-8745-C15FDDB83ACE_11zon.jpeg",
+				"caption": "<i>Top of the World, reaching the summit</i>",
+				"credit": ""
+			},
+			"location": {
+				"lat": 33.605704437517375,  
+				"line": false,
+				"lon": -117.77559563479998,
+				"zoom": 19
+			},
+			"text": {
+				"headline": "<h6 style='color:red;'>Top of the World</h6>",
+				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+			}
+			},
+			{
+			"date": "",
+			"media": {
 				"url": "images/IMG_8644_11zon.jpg",
 				"caption": "<i>Top of the World, reaching the summit</i>",
 				"credit": ""
