468eca23d8c78e1d3fd61604f58d5a40b856dcaa
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-10 09:30:30 -0800

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -131,7 +131,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Enjoy the Views</h6>",
-				"text": "Enjoy the views before starting a descend. There will still be ocean views as you continue, but you will need to focus more on footing as you go downhill. The image taken is on the left side of the bush that you will run into."
+				"text": "Enjoy the views before starting the next section. There will still be ocean views as you continue, but you will need to focus more on footing in the next section as you go downhill. This image taken is also on the left side of the bush that you will run into right before the start of the  next section."
 			}
 			},
 			{
