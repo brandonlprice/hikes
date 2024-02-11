c1b6953311e4db294a268e84893e2492c15949e0
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-11 12:23:12 -0800

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.5 miles (Out and Back)</li><li>One Moderate Steep Section</li><li>No Shade</li><li>Parking</li><li>Trail Status</li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.5 miles (Out and Back)</li><li>One moderate steep section and mainly no shade.</li><li>Parking</li><li>Trail Status</li></ul></small>",
 				"text": "Park at the end of Talavera Dr right before it runs into the gated community. The trail starts at Seaview Park in Laguna Niguel and ends in Laguna Beach at Aliso Peak."
 			}
 			},
