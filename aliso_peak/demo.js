9d5851b1682365405768a6ab705c85e1308f76dc
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-16 01:10:54 -0800

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -203,7 +203,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Going Downhill Continued</h6>",
-				"text": "Another shot going downhill, taken on a different day. Trail conditions vary depending on when trail maintenance work was last done."
+				"text": "Another shot going downhill on a different day. Trail conditions vary depending on when trail maintenance work was last done."
 			}
 			},
 			{
