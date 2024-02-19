d23f923f151a3299c70206a959e8c4fac0fd9ee8
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-19 08:28:41 -0800

    Update demo.js

--- badlands/demo.js
+++ badlands/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.5 miles</li><li>Out & Back</li><li>No Shade</li><li>Park at <u>31716-31626 Isle Vista, Laguna Niguel, CA 92677↗</u></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.5 miles</li><li>Out & Back</li><li>No Shade</li><li>Park at <u><small><a href='https://maps.app.goo.gl/YZHAAhFqjfUbBmGX9'>31716-31626 Isle Vista Laguna Niguel, CA 92677↗</a></small></u></li></ul></small>",
 				"text": "Street parking on Isle Vista. Two ADA parking spots available directly next to park."
 			}
 			},
