d8ee0dd47ad6df6b225ef42ea5ad7f9e07c4dfde
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-19 13:52:25 -0800

    Update demo.js

--- barbara_lake/demo.js
+++ barbara_lake/demo.js
@@ -23,7 +23,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>Barbara's Lake, Laguna Coast Wilderness</h4>",
-				"text": "This trail takes you to one of the most visited scenic destinations in Orange County called \"Top of the World\". It's known for being one of the highest elevation points with such a close proximity to the ocean in the county. It stands slightly above 1,000 feet. And even though it's not as tall as the 2,000 plus feet Santa Monica Mountains right off the Malibu Coast in LA County, it's still worth checking out. There are four main ways to get there: this way which starts in Aliso Viejo, the Canyon Acres Trail in Laguna Beach, and another unnamed trail in Laguna Beach, and driving there."
+				"text": "This is one of the 3 natural occuring lakes in Orange County which are all located within a quarter-mile from each other in Laguna Canyon. This is the only one that cannot be seen from Laguna Canyon Road."
 			}
 			},
 			{
