c1a6bff12554cbf272e151e5354f0fe8363a5547
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-25 13:21:33 -0800

    Update demo.js

--- billygoat-stairmasters/demo.js
+++ billygoat-stairmasters/demo.js
@@ -22,8 +22,8 @@ let demojs={
 				"line": false
 			},
 			"text": {
-				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>Borrego Cyn, Billygoat, & Mustard Trails</h4>",
-				"text": "There are three popular trails that all can be accessed from this entrance with the most popular being the Red Rock Canyon Trail to see the red rocks. Another trail, however, is the Billygoat Trail. It is equally unique and a bit more challenging. Stairs were built into the hills so it gives you an intense stairmaster type of workout."
+				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>Borrego Cyn Trail to Billygoat Stairs Trail</h4>",
+				"text": "There are three popular trails that all can be accessed from the Borrego Canyon Trail with the most popular being the Red Rock Canyon Trail to see the red rocks. Another trail, however, is the Billygoat Trail. It is equally unique and a bit more challenging. Stairs were built into the hills so it gives you an intense stairmaster type of workout."
 			}
 			},
 			{
