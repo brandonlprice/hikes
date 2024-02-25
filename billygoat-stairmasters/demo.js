e365909b115867a072571788a978358b79ab4711
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-02-25 09:15:01 -0800

    Update demo.js

--- billygoat-stairmasters/demo.js
+++ billygoat-stairmasters/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>5 miles</li><li>Intermittent Steep Sections</li><li>Shaded Relief</li><li>Lollipop Loop</li><li>Park at <a href='https://maps.app.goo.gl/8hGRbycmvGShypZM6'><font size=4>26701 Portola Pkwy</font></a> or <font size=4>26722 Portola Pkwy</font> in Lake Forest, CA.</li><li><u>Check for Alerts</u></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>5 miles (Lollipop Loop)</li><li>Intermittent Steep Sections</li><li>Shaded Relief</li><li>Park at <a href='https://maps.app.goo.gl/8hGRbycmvGShypZM6'><font size=5><i>26701 Portola Pkwy</i></font></a> or <font size=5><i>26722 Portola Pkwy</i></font> in Lake Forest, CA.</li><li><u>Check for Alerts</u></li></ul></small>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
