465a698826ffc44fa0ac6d5480746c026ff07cab
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-02 16:30:50 -0800

    Update demo.js

--- bolsa_marsh/demo.js
+++ bolsa_marsh/demo.js
@@ -256,7 +256,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Stay Straight</h6>",
+				"headline": "<h6 style='color:red;'>Go Straight</h6>",
 				"text": "After the last uphill you should see the Top of the World in the distance. At this point it should be fairly flat the last quarter of a mile until getting to Top of the World. There is a nice view looking back which you can check, otherwise you'll see it later on going back."
 			}
 			},
