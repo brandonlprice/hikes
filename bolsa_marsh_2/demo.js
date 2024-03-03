9a8fa7405394d7ccfff99ed69f76224e4e4610c9
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-02 16:41:32 -0800

    Update demo.js

--- bolsa_marsh_2/demo.js
+++ bolsa_marsh_2/demo.js
@@ -76,7 +76,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 miles</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Out & Back</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Veer Right</h6>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
