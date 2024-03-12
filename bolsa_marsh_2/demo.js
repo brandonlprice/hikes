582b74e73181f64a8d0d366e47e05e741d9d9cea
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-12 16:47:54 -0700

    Update demo.js

--- bolsa_marsh_2/demo.js
+++ bolsa_marsh_2/demo.js
@@ -131,7 +131,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Enjoy the Views</h6>",
-				"text": "Continue straight on this trail all the way down to Top of the World while enjoying the views along the way. You'll see the 133 on your right way down below and nice views of Wood Canyon on your left."
+				"text": "Continue on the trail when ready."
 			}
 			},
 			{
