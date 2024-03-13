cfc2e52ee0bfe9c8b4b242c95dc0bd89de040e6a
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-13 06:43:55 -0700

    Update demo.js

--- bolsa_marsh_2/demo.js
+++ bolsa_marsh_2/demo.js
@@ -257,7 +257,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Go Straight</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"text": "Go straight a little bit further until you get to the opening between the wooden fence board."
 			}
 			},
 			{
