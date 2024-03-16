29995f2292adc574ccce402165410f811ab122a5
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-16 06:40:23 -0700

    Update demo.js

--- bommer_cyn/demo.js
+++ bommer_cyn/demo.js
@@ -184,7 +184,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
+				"headline": "<h6 style='color:red;'>Small Valley</h6>",
 				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
 			}
 			},
