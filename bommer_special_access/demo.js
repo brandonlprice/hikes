833a47eddd16053f160619f45ff01a70edeae2ba
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-26 12:49:00 -0700

    Update demo.js

--- bommer_special_access/demo.js
+++ bommer_special_access/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 mile loop</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Bommer Canyon Rd Entrance</h6> <small style='color:red;'><ul><li>4 mile loop</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
@@ -58,7 +58,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 mile loop</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Bommer Canyon Rd</h6>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
@@ -76,7 +76,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 mile loop</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Turn Right</h6>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
@@ -94,7 +94,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 mile loop</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Parking Lot</h6>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
