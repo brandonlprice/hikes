b137233519c7f2abf03a91659e7f09cd5924f50e
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-03-31 07:57:10 -0700

    Update demo.js

--- bommer_special_access/demo.js
+++ bommer_special_access/demo.js
@@ -112,8 +112,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Starting Point:</h6> <small style='color:red;'><ul><li>4 mile loop</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
-				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
+				"headline": "<h6 style='color:red;'>Starting Point:</h6> <small style='color:red;'><ul><li>3.4 mile loop</li><li>Steep sections, Brief shade relief</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
+				"text": "Walk back towards the parking entrance after parking."
 			}
 			},
 			{
