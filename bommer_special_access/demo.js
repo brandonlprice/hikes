2d482160a0fe4b4ae050aefa6cd398f87cc70f6a
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-01 05:03:34 -0700

    Update demo.js

--- bommer_special_access/demo.js
+++ bommer_special_access/demo.js
@@ -112,8 +112,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>3.4 mile loop</li><li>Steep sections, Brief shade relief</li><li>Duration: 1 ½ hours</li><li>Park at 11 Bommer Canyon · Irvine, CA 92603</li></ul></small>",
-				"text": "Walk back towards the parking entrance after parking (Park Rangers have maps available for pickup their parking lot booth on Wilderness Access Days)"
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>3.4 mile loop</li><li>Steep sections, Brief shade relief</li><li>Duration: 1 ½ hours</li><li>Park at <a href='https://maps.app.goo.gl/m1DT2ZTa5PXGGjrY7' target='_blank'>11 Bommer Canyon · Irvine, CA 92603↗</a></li></ul></small>",
+				"text": "Walk back towards the parking entrance after parking (park rangers have maps available for pickup at their parking lot booth on Wilderness Access Days)"
 			}
 			},
 			{
