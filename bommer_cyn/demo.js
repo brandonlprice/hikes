650bd9ff2333d76c24c3a61628902082d48de3d1
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-05 10:11:31 -0700

    Update demo.js

--- bommer_cyn/demo.js
+++ bommer_cyn/demo.js
@@ -14,7 +14,7 @@ let demojs={
 			"date": "Jan 2006",
 			"media": {
 				"url": "images/IMG_8153_11zon.jpg",
-				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
+				"caption": "<i>Bommer Canyon, Irvine, CA, Summer 2022</i>",
 				"credit": ""
 			},
 			"type": "overview",
