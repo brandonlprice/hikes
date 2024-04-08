40d9365c38db670c1342062e76add3ab6d395bf7
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-08 05:59:04 -0700

    Update demo.js

--- buck_gully/demo.js
+++ buck_gully/demo.js
@@ -23,7 +23,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h4 style='color: red;'>Buck Gully Loop</h4>",
-				"text": "The Buck Gully trail is located in the Newport Coast neighborhood of Newport Beach and managed by the city of Newport Beach. This trail is an out and back, but can also be done in a loop by taking other trails like in this example."
+				"text": "The Buck Gully trail is located in the Newport Coast neighborhood of Newport Beach and is managed by the city of Newport Beach. This trail is an out and back, but can also be done in a loop by taking other trails like in this example."
 			}
 			},
 			{
