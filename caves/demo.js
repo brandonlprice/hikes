0181d36ecbff644fe1e205301b1558cb179f9d2b
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-18 06:08:45 -0700

    Update demo.js

--- caves/demo.js
+++ caves/demo.js
@@ -300,7 +300,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_1613_11_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -310,8 +310,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Cross the Bridge</h6>",
+				"text": "Cross the bridge. More trees begin to pop up."
 			}
 			},
 			{
