a1e3bb0446f164a3e0e9b3693b585224188b03a8
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-18 06:16:10 -0700

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -148,7 +148,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Entering Downhill</h6>",
+				"headline": "<h6 style='color:red;'>Enter Downhill Section</h6>",
 				"text": "Enter the downhill section between these shrubs. The trail will change and be very narrow."
 			}
 			},
