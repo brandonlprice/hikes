5b43eabe1d72127ada25c2aaf69b1fa127bf2222
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-25 05:43:36 -0700

    Update demo.js

--- hb-central-park-west/demo.js
+++ hb-central-park-west/demo.js
@@ -102,7 +102,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_1304_11zon.jpg",
-				"caption": "<i>Views of Wood Canyon to the east.</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -112,8 +112,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Enjoy the Views</h6>",
-				"text": "Continue straight on this trail all the way down to Top of the World while enjoying the views along the way. You'll see the 133 on your right way down below and nice views of Wood Canyon on your left."
+				"headline": "<h6 style='color:red;'>Turn Right</h6>",
+				"text": "Turn right towards the lake when the fencing ends."
 			}
 			},
 			{
