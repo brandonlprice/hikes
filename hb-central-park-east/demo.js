6450eb3ce42fc5817a5f8f0b3e841fc2dbf252f7
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-24 18:45:30 -0700

    Update demo.js

--- hb-central-park-east/demo.js
+++ hb-central-park-east/demo.js
@@ -354,7 +354,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/D0A21102-D2F7-4147-BF31-C9A28D2BE4BF_11zon.jpeg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -364,8 +364,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Pass Maintenance Area</h6>",
+				"text": "Trees on the left serve as a barrier from the street, but still can be partially be seen in certain areas."
 			}
 			},
 			{
