7780ba3555e5b78d207db20e9367d33259447cf8
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-04-28 08:04:40 -0700

    Update demo.js

--- horseshoe-puma-ridge-loop/demo.js
+++ horseshoe-puma-ridge-loop/demo.js
@@ -14,7 +14,7 @@ let demojs={
 			"date": "Jan 2006",
 			"media": {
 				"url": "images/D4A09283-1B48-4D37-B07A-22C011394857_11zon.jpeg",
-				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
+				"caption": "<i>Viewpoint on Puma Ridge, Irvine Regional Park, Spring 2023</i>",
 				"credit": ""
 			},
 			"type": "overview",
@@ -22,8 +22,8 @@ let demojs={
 				"line": false
 			},
 			"text": {
-				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>West Ridge Trail to the Top of the World</h4>",
-				"text": "This trail takes you to one of the most visited scenic destinations in Orange County called \"Top of the World\". It's known for being one of the highest elevation points with such a close proximity to the ocean in the county. It stands slightly above 1,000 feet. And even though it's not as tall as the 2,000 plus feet Santa Monica Mountains right off the Malibu Coast in LA County, it's still worth checking out. There are four main ways to get there: this way which starts in Aliso Viejo, the Canyon Acres Trail in Laguna Beach, and another unnamed trail in Laguna Beach, and driving there."
+				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>Horshoe/Puma Ridge Loop</h4>",
+				"text": "This hike is located in Orange County's oldest park, Irvine Regional Park (est. 1897). You will get nice views of the park and Santa Ana Mountains from the trails taken. "
 			}
 			},
 			{
