0b7e5237f1f7891effc9c67fffeadbfb486b7a23
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-01 13:09:58 -0700

    Update demo.js

--- horseshoe-puma-ridge-loop/demo.js
+++ horseshoe-puma-ridge-loop/demo.js
@@ -246,7 +246,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_9166_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -256,15 +256,15 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Moderate Incline</h6>",
+				"text": "The incline here is a little steeper than the first incline at the start of the hike."
 			}
 			},
 			{
 			"date": "",
 			"media": {
 				"url": "images/IMG_9176_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -274,15 +274,15 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Switchback</h6>",
+				"text": "Halfway into the ascend, the trail will start winding back and forth up the hill to make the incline less steep."
 			}
 			},
 			{
 			"date": "",
 			"media": {
 				"url": "images/IMG_9181_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -292,8 +292,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Narrow and Brushy</h6>",
+				"text": "The trail is narrow the whole way up. THe brush may or may not be as thick in the picture depending on when the trail was maintained."
 			}
 			},
 			{
