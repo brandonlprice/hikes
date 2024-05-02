c3178d419c3781df76e6f0fe82fadbded6398d94
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-02 14:18:37 -0700

    Update demo.js

--- live-oak-nature-dr/demo.js
+++ live-oak-nature-dr/demo.js
@@ -14,7 +14,7 @@ let demojs={
 			"date": "Jan 2006",
 			"media": {
 				"url": "images/IMG_7264_11zon.jpg",
-				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
+				"caption": "<i>Live Oak Cyn Dr, Trabuco Caynon, CA, Summer 2022</i>",
 				"credit": ""
 			},
 			"type": "overview",
@@ -22,7 +22,7 @@ let demojs={
 				"line": false
 			},
 			"text": {
-				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>West Ridge Trail to the Top of the World</h4>",
+				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>Nature Drive: Live Oak Canyon Dr to O'Neill Regional Park</h4>",
 				"text": "This trail takes you to one of the most visited scenic destinations in Orange County called \"Top of the World\". It's known for being one of the highest elevation points with such a close proximity to the ocean in the county. It stands slightly above 1,000 feet. And even though it's not as tall as the 2,000 plus feet Santa Monica Mountains right off the Malibu Coast in LA County, it's still worth checking out. There are four main ways to get there: this way which starts in Aliso Viejo, the Canyon Acres Trail in Laguna Beach, and another unnamed trail in Laguna Beach, and driving there."
 			}
 			},
