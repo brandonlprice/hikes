aff8fc7f004ce544270e02d57a5a07b6006f2fde
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-04 12:10:54 -0700

    Update demo.js

--- longview_condensed/demo.js
+++ longview_condensed/demo.js
@@ -14,7 +14,7 @@ let demojs={
 			"date": "Jan 2006",
 			"media": {
 				"url": "images/IMG_8875_11zon.jpg",
-				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
+				"caption": "<i>Longview Park, Laguna Niguel, Fall 2021</i>",
 				"credit": ""
 			},
 			"type": "overview",
