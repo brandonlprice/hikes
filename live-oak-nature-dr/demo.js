208b639ca9e83437558f3253030d0168b691176a
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-04 09:02:08 -0700

    Update demo.js

--- live-oak-nature-dr/demo.js
+++ live-oak-nature-dr/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>5.35 miles</li><li>Windy Treelined Streets Leading to a Small Summit</li><li>Duration: 30 minutes</li><li><a href='https://maps.app.goo.gl/aPWj7mW7x9XfhH1FA' target='_blank'><i>Start at the Live Oak Cyn Rd/El Toro Rd Intersection↗</i></a></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>5.35 miles</li><li>Winding Treelined Streets Leading to a Small Summit</li><li>Duration: 30 minutes</li><li><a href='https://maps.app.goo.gl/aPWj7mW7x9XfhH1FA' target='_blank'><i>Start at the Live Oak Cyn Rd/El Toro Rd Intersection↗</i></a></li></ul></small>",
 				"text": "Start here on Live Oak Canyon Dr heading east. It will be a ten minute treelined drive that takes you too O'Neill Regional Park. From there, a $3-5 fee is required to enter. Upon entering, the nature drive continues and ends at a small summit."
 			}
 			},
@@ -174,7 +174,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_7280_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -184,8 +184,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Keep on Main Road</h6>",
+				"text": "Avoid turning on the side streets within the park. The main road will start veering left."
 			}
 			},
 				{
