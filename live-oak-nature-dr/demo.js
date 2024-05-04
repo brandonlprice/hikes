8f8efbeac72b508844b128a4c9e8a0ebc5a83e9f
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-04 09:05:57 -0700

    Update demo.js

--- live-oak-nature-dr/demo.js
+++ live-oak-nature-dr/demo.js
@@ -210,7 +210,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/FCC7F46B-C05B-4745-9791-C367762ED8F4_11zon.jpeg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -220,8 +220,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Roundabout</h6>",
+				"text": "Continue on the roundabout."
 			}
 			},
 			{
