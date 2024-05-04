8ff7a724396dd002bf0f44cae952e3941a0da335
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-04 09:18:56 -0700

    Update demo.js

--- live-oak-nature-dr/demo.js
+++ live-oak-nature-dr/demo.js
@@ -274,8 +274,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Cross the Wash</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Trabuco Creek</h6>",
+				"text": "Cross the creek. The road to cross is usually dry but can fill with water when it rains."
 			}
 			},
 			{
