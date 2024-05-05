107491488eb8fe13ef31dcaa535b9e1fecffe1af
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-05 04:00:53 -0700

    Update demo.js

--- longview_condensed/demo.js
+++ longview_condensed/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>Half-Mile Round Trip</li><li>Short Uphill With Shade to a Summit</li><li>Enter Gated Community at <a href='https://maps.app.goo.gl/6F8ea4r3xo7hpz9H6' target='_blank'><i>Pacific Crest↗</i></a></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>Half-Mile Round Trip</li><li>Short Uphill With Shade to a Summit</li><li>Enter Gated Community on <a href='https://maps.app.goo.gl/6F8ea4r3xo7hpz9H6' target='_blank'><i>Pacific Crest↗</i></a></li></ul></small>",
 				"text": "The park you start the hike at is within a private community. Press the black button pictured and the gate will open to enter into the community. The button is disabled after 5pm."
 			}
 			},
