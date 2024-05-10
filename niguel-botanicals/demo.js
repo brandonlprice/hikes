6867c2fa66cc20fd4b9a24acfae08c407bff97df
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-10 02:03:52 -0700

    Update demo.js

--- niguel-botanicals/demo.js
+++ niguel-botanicals/demo.js
@@ -156,7 +156,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_9327_11zon.jpg",
-				"caption": "<i>Entering Top of the World</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -166,8 +166,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Entering the Summit</h6>",
-				"text": "Keep going straight until the trail ends. You'll feel like you're leaving the park and entering back into city as you should see more people and a street in front of you. Rather than taking the street, loop around to the right for the short trek up to the Top of the World."
+				"headline": "<h6 style='color:red;'>Exit Detour</h6>",
+				"text": "Take the sidewalk here to exit the detour area and re-join the main path."
 			}
 			},
 			{
