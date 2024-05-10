dfc8283013a7009e793c56932388b44662d5d6c3
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-10 02:20:23 -0700

    Update demo.js

--- niguel-botanicals/demo.js
+++ niguel-botanicals/demo.js
@@ -210,7 +210,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_9336_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -220,8 +220,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Overlook</h6>",
+				"text": "A nice clear overlook where the vegetation is shorter. The decline start after this. There are benches to rest at before continuimg."
 			}
 			},
 			{
