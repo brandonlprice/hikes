de274a04d88aa1059d65c7b0affc57e437395b18
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-11 04:44:48 -0700

    Update demo.js

--- oso-creek/demo.js
+++ oso-creek/demo.js
@@ -192,7 +192,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_7750_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<it</i>",
 				"credit": ""
 			},
 			"location": {
@@ -202,8 +202,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Turn Left</h6>",
+				"text": "Turn left again at the junction. The tunnel underpass will be on the right."
 			}
 			},
 			{
