6002f7168ffe78cc37ae02c2be0c93b27fb81e70
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-14 13:28:03 -0700

    Update demo.js

--- quail-trail/demo.js
+++ quail-trail/demo.js
@@ -149,7 +149,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Uphill</h6>",
-				"text": "Keep going uphill. Is is moderately steep."
+				"text": "Keep going uphill. It is moderately steep."
 			}
 			},
 			{
