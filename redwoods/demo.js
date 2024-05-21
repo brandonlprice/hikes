0b77c6032f1de393e7d8ede1b70f042293d9de43
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-21 04:21:52 -0700

    Update demo.js

--- redwoods/demo.js
+++ redwoods/demo.js
@@ -41,7 +41,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>.75 miles</li><li>Short Distance, Flat, No shade until reaching the Redwoods</li><li>Park Entrance Fee of $3 (weekdays)/$5 (weekends)</li><li><a href='https://www.ocparks.com/parks-trails/carbon-canyon-regional-park' target='_blank'><i>Check for Alerts</i></a></li><li>Park in <a href='https://maps.app.goo.gl/GKRWQQ7n7eWdW44j9' target='_blank'><i>Lot A (southernmost lot)</i></a></li></ul></small>",
-				"text": "This lot usually will have spaces available even on the weekends. Many people will be out on trails as this is a popular desitination people regularly use for photo backdrops."
+				"text": "There is usually parking available even on the weekends. Many people come here as this is a popular destination used for photo backdrops."
 			}
 			},
 			{
