cf70c1d1d6aebed37a26b8584fb42a544e1381e4
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-21 04:30:55 -0700

    Update demo.js

--- redwoods/demo.js
+++ redwoods/demo.js
@@ -30,7 +30,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_6932_11zon.jpg",
-				"caption": "Lot A",
+				"caption": "Lot A (southernmost lot)",
 				"credit": ""
 			},
 			"location": { 
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>.75 miles</li><li>Short Distance, Flat, No shade until reaching the Redwoods</li><li>Park Entrance Fee of $3 (weekdays)/$5 (weekends)</li><li><a href='https://www.ocparks.com/parks-trails/carbon-canyon-regional-park' target='_blank'><i>Check for Alerts</i></a></li><li>Park in <a href='https://maps.app.goo.gl/GKRWQQ7n7eWdW44j9' target='_blank'><i>Lot A (southernmost lot)</i></a></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>.75 miles</li><li>Short Distance, Flat, No shade until reaching the Redwoods</li><li>Park Entrance Fee of $3 (weekdays)/$5 (weekends)</li><li><a href='https://www.ocparks.com/parks-trails/carbon-canyon-regional-park' target='_blank'><i>Check for Alerts</i></a></li><li>Park in <a href='https://maps.app.goo.gl/GKRWQQ7n7eWdW44j9' target='_blank'><i>Lot A</i></a></li></ul></small>",
 				"text": "There is usually parking available even on the weekends. Many people come here as this is a popular destination used for photo backdrops."
 			}
 			},
