b8b83d62a941c35ffe43c8aef35d564bd09d44ad
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-21 05:59:57 -0700

    Update demo.js

--- redwoods/demo.js
+++ redwoods/demo.js
@@ -59,7 +59,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Starting Point</h6>",
-				"text": "Once parked, this is where you'll start. This isn't the official trail yet. Just an access route into Aliso & Wood Canyons Regional Park. It's all fairly steep and uphill, but a short distance."
+				"text": "Once parked, this is where you'll start. It's on the right side of the parking lot and will have a sign that says to Redwood Grove."
 			}
 			},
 			{
