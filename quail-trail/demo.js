b66460474fb83212a863d33867824c0df42ed0b6
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-24 03:22:21 -0700

    Update demo.js

--- quail-trail/demo.js
+++ quail-trail/demo.js
@@ -318,7 +318,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_0452_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -328,8 +328,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Cross the Street</h6>",
+				"text": "Cross the street to get to the other side and then go left."
 			}
 			},
 					{
