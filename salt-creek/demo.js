2b65770c3462458cca39eaa4f20ba90699407a13
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-25 19:08:22 -0700

    Update demo.js

--- salt-creek/demo.js
+++ salt-creek/demo.js
@@ -40,7 +40,7 @@
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.25 mile loop</li><li>Unshaded multi-use trail with modest hills</li><li>Park at <a href='https://maps.app.goo.gl/5xpqAzdpGaa5eRt66'><i>Pooch Park Parking</i></a></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.25 mile loop</li><li>Unshaded multi-use trail with modest hills</li><li>Park at <a href='https://maps.app.goo.gl/5xpqAzdpGaa5eRt66'><i>Pooch Park Parking↗</i></a></li></ul></small>",
 				"text": "Park at in the free public parking lot off Golden Lantern Boulevard. It has a sign that says Pooch Park Parking. The lot is used by people using the trails and for the dog park nearby."
 			}
 			},
