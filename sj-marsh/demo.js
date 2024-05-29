24b429d99790ddc81e1d3b63daeb2b683bc296a3
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-29 05:08:31 -0700

    Update demo.js

--- sj-marsh/demo.js
+++ sj-marsh/demo.js
@@ -256,7 +256,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Go straight</h6>",
+				"headline": "<h6 style='color:red;'>Trail Split</h6>",
 				"text": "Stay on the trail to the right."
 			}
 			},
@@ -400,8 +400,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Veering Right</h6>",
+				"text": "The trail will naturally veer right."
 			}
 			},
 			{
