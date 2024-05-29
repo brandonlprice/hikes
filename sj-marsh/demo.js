7404da311ccc51edc4813787d539e2e6a13315fb
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-29 03:13:59 -0700

    Update demo.js

--- sj-marsh/demo.js
+++ sj-marsh/demo.js
@@ -30,7 +30,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_7048_11zon.jpg",
-				"caption": "Turn left here to park",
+				"caption": "",
 				"credit": ""
 			},
 			"location": { 
@@ -41,7 +41,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>1.2 mile loop</li><li>Flat & Open Hike Around Multiple Ponds</li><li>No Shade</li><li>Park at the <a href='https://maps.app.goo.gl/TdMhCnGB6eHf1pzE6' target='_blank'><i>The Duck Club↗</i></a></li></ul></small>",
-				"text": "Turn left where the houses and people are to park. If it's full there is additional parking to the right."
+				"text": "Turn left into the opening with the houses and people to park. If it's full there is additional parking to the right."
 			}
 			},
 			{
