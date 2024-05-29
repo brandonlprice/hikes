3d0e97c3c48024b06f6a7dc1bd50a8e7a7e919cc
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-29 04:44:47 -0700

    Update demo.js

--- sj-marsh/demo.js
+++ sj-marsh/demo.js
@@ -274,7 +274,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Second Pond</h6>",
+				"headline": "<h6 style='color:red;'>Go Straight</h6>",
 				"text": "Keep going straight passing the trail on the right."
 			}
 			},
@@ -282,7 +282,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/8BD885CA-B2F7-40C3-9B4B-FAA8D896BD20_11zon.jpeg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "<i></i>",
 				"credit": ""
 			},
 			"location": {
@@ -292,8 +292,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Pond</h6>",
+				"text": "A view of another pond. Continue straight."
 			}
 			},
 			{
