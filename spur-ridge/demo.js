efcf3dadc00891ea5fd03d8e564dec43096657d9
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-30 18:19:50 -0700

    Update demo.js

--- spur-ridge/demo.js
+++ spur-ridge/demo.js
@@ -220,8 +220,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Go straight</h6>",
+				"text": "There is another trail on the right side. Stay on the trail pictured."
 			}
 			},
 			{
