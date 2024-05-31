e86d59e2dfc5c9caa3ffc094e872464e5605457f
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-31 07:26:08 -0700

    Update demo.js

--- longview/demo.js
+++ longview/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>4 miles</li><li>Intermittent Steep Sections</li><li>No Shade</li><li>Out & Back</li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>3.2 miles (Out & Back)</li><li>Hillside trail, Pockets of shade, Ocean view</li><li>Park in the<a href='https://maps.app.goo.gl/3uKY3ZWjnizcK67M8' target='_blank'><i>Pooch Park Parking Lot↗</i></a></li></ul></small>",
 				"text": "This is where you park. Along Hollyeaf where it dead ends into a cul-de-sac. It's better to park on the side with no houses out of courtesy to residents. It shouldn't be full."
 			}
 			},
