6b31607894663a25552be1b78a39c8ae36f7a713
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-31 03:21:26 -0700

    Update demo.js

--- spur-ridge/demo.js
+++ spur-ridge/demo.js
@@ -102,7 +102,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_2369_11zon.jpg",
-				"caption": "<i>Views of Wood Canyon to the east.</i>",
+				"caption": "",
 				"credit": ""
 			},
 			"location": {
@@ -112,8 +112,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Enjoy the Views</h6>",
-				"text": "Continue straight on this trail all the way down to Top of the World while enjoying the views along the way. You'll see the 133 on your right way down below and nice views of Wood Canyon on your left."
+				"headline": "<h6 style='color:red;'>Gradual Incline</h6>",
+				"text": "The uphill flattens or decreaes a little here. A canyon is to the left of the trail."
 			}
 			},
 			{
