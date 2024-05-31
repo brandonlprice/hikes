69b85b50dc8d3e80d6cc070fb16ccdf5fe6729b9
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-31 02:38:17 -0700

    Update demo.js

--- spur-ridge/demo.js
+++ spur-ridge/demo.js
@@ -156,7 +156,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_2379_11zon.jpg",
-				"caption": "<i>One out of of a handful of steep inclines.</i>",
+				"caption": "<i>",
 				"credit": ""
 			},
 			"location": {
@@ -166,8 +166,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Steep Inclines</h6>",
-				"text": "As you get closer to the destination, be aware that you'll encounter very short-lived yet very steep inclines. These inclines do not seem as tough on your legs on the way back though. Mountain bikers often will be present on the trail using these inlines to practice going uphill. Take the uphill going right to stay on the West Ridge Trail rather than taking the hard left down Mathis Canyon."
+				"headline": "<h6 style='color:red;'>Minor Ascension</h6>",
+				"text": "A short-lived ascend with partial view of the ocean still."
 			}
 			},
 			{
