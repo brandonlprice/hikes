6e37573f0775f17df634d919b363054c419c2ffe
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-05-31 06:48:10 -0700

    Update demo.js

--- longview/demo.js
+++ longview/demo.js
@@ -23,7 +23,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h4 style='color: red;font-size:.9em;line-height: .92em;'>Longview Trl to Longview Park</h4>",
-				"text": "This trail takes you to one of the most visited scenic destinations in Orange County called \"Top of the World\". It's known for being one of the highest elevation points with such a close proximity to the ocean in the county. It stands slightly above 1,000 feet. And even though it's not as tall as the 2,000 plus feet Santa Monica Mountains right off the Malibu Coast in LA County, it's still worth checking out. There are four main ways to get there: this way which starts in Aliso Viejo, the Canyon Acres Trail in Laguna Beach, and another unnamed trail in Laguna Beach, and driving there."
+				"text": "This trail travels along the edge of a mansion community in Laguna Niguel leading to small summit with an ocean view called Longview Park. Trail access to the park is blocked, however. The hike either can be ended at another summit before reaching Longview Park or a makeshift route can be taken to the park."
 			}
 			},
 			{
