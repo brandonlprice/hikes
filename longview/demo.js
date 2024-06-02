03311d4bf20bef74f33315cf31ce2f79d7097ba1
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-06-02 09:17:13 -0700

    Update demo.js

--- longview/demo.js
+++ longview/demo.js
@@ -138,7 +138,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_9237_11zon.jpg",
-				"caption": "<i>Nice view looking back at all the ground covered.</i>",
+				"caption": "",
 				"credit": ""
 			},
 			"location": {
@@ -148,8 +148,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Nearing the End</h6>",
-				"text": "After the last uphill you should see the Top of the World in the distance. At this point it should be fairly flat the last quarter of a mile until getting to Top of the World. There is a nice view looking back which you can check, otherwise you'll see it later on going back."
+				"headline": "<h6 style='color:red;'>Modest Uphill</h6>",
+				"text": "An incline will ensue after veering left up a hill."
 			}
 			},
 			{
