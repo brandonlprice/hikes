7be57251b4de675f6da8c9863c4b4c9d1700ab95
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-06-07 00:50:29 -0700

    Update demo.js

--- redrock-cyn/demo.js
+++ redrock-cyn/demo.js
@@ -14,7 +14,7 @@ let demojs={
 			"date": "Jan 2006",
 			"media": {
 				"url": "images/IMG_7978_11zon.jpg",
-				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
+				"caption": "<i>Red Rock Canyon, Whiting Ranch Wilderness, Foothill Ranch, Summer 2022</i>",
 				"credit": ""
 			},
 			"type": "overview",
