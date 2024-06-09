4890b91c42bb6213e3b49722588d0b9ec6aca46a
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-06-09 08:04:04 -0700

    Update demo.js

--- redrock-cyn/demo.js
+++ redrock-cyn/demo.js
@@ -77,7 +77,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "<h6 style='color:red;'>Starting Point</h6>",
-				"text": "Once parked, this is where you'll start. Hike in groups. There are signs about warning about mountain lions."
+				"text": "Once parked, this is where you'll start, it's the Borrego Trail. Hike in groups. There are signs about warning about mountain lions."
 			}
 			},
 			{
@@ -94,8 +94,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The West Ridge Trail</h6>",
-				"text": "Once at the top, you are now within the county park and at the trailhead for West Ridge. You'll stay on this same trail for the next 2 miles until reaching your destination. Be sure to stay on this trail and not accidentally take another park trail. This can happen since the trails have no signage at some junctions. However, since this trail is a ridge trail, if you inadvertently take the wrong trail, you would know because you would be declining off the ridge."
+				"headline": "<h6 style='color:red;'>Straight Shot</h6>",
+				"text": "A straights away. This trail will have some winding later on."
 			}
 			},
 			{
