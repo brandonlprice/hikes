9a871c5feff8300baeea1e953fe4701417ad5320
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-06-12 01:21:41 -0700

    Update demo.js

--- redrock-cyn/demo.js
+++ redrock-cyn/demo.js
@@ -174,7 +174,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_7947_11zon.jpg",
-				"caption": "<i>Nice view looking back at all the ground covered.</i>",
+				"caption": "",
 				"credit": ""
 			},
 			"location": {
@@ -184,8 +184,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Nearing the End</h6>",
-				"text": "After the last uphill you should see the Top of the World in the distance. At this point it should be fairly flat the last quarter of a mile until getting to Top of the World. There is a nice view looking back which you can check, otherwise you'll see it later on going back."
+				"headline": "<h6 style='color:red;'>Closing In</h6>",
+				"text": "The canyon begins to narrow as the hills close in on the trail."
 			}
 			},
 			{
