413b1f997aa2796e1fbb0cb9d5b22c92d5757e40
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-06-12 04:00:44 -0700

    Update demo.js

--- redrock-cyn/demo.js
+++ redrock-cyn/demo.js
@@ -300,7 +300,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_7978_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "",
 				"credit": ""
 			},
 			"location": {
@@ -310,8 +310,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Final Destination</h6>",
+				"text": "You have reached the end of the canyon. Head back when ready."
 			}
 			}
 		]
