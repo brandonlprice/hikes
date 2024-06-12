07dd207eec87678c2ae3859130c60bd53600fc7f
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-06-12 02:51:21 -0700

    Update demo.js

--- aliso_summit/demo.js
+++ aliso_summit/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>5.2 miles (Out & Back)</li><li>No shade, Gradual Elevation Gain</li><li>Duration: 2 hrs</li><li><u>Park at</u></li><li><u>Check for Alerts</u></li></ul></small>",
+				"headline": "<h6 style='color:red;'>The Rundown:</h6> <small style='color:red;'><ul><li>5.2 miles (Out & Back)</li><li>No shade, Gradual elevation gain, Ocean views</li><li>Duration: 2 hrs</li><li>Park on <a href='https://maps.app.goo.gl/Dqnk1bjsEKhx2tfx5' target='_blank'><i>Ridgview Dr near Highlands Ave↗</i></a></li><li><u>Check for Alerts</u></li></ul></small>",
 				"text": "Residential street parking is on Ridgeview Drive."
 			}
 			},
