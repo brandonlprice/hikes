6e5b65825234d8eb20008b112de3dab79515b2f2
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-03 06:11:41 -0700

    Update demo.js

--- serrano-ridge/demo.js
+++ serrano-ridge/demo.js
@@ -461,7 +461,7 @@ let demojs={
 			"date": "",
 			"media": {
 				"url": "images/IMG_8712_11zon.jpg",
-				"caption": "<i>Top of the World, reaching the summit</i>",
+				"caption": "",
 				"credit": ""
 			},
 			"location": {
@@ -471,8 +471,8 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Top of the World</h6>",
-				"text": "Once there, soak in the 360° views. You should see the Santa Ana Mountains to the north and the ocean to the south. Elevation is 1,036 feet. Nothing within several miles is taller than this point. Take the same way back."
+				"headline": "<h6 style='color:red;'>Go Past Fence</h6>",
+				"text": "Go around or step over the small fence barrier seen in the distance. It's purpose is not known."
 			}
 			},
 			{
