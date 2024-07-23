807bea994a6112d1d6d87df97a3611677b11a6c5
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-23 13:04:59 -0700

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -40,7 +40,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Parking</h6><ul><small style='color:red;'><li><a href='https://maps.app.goo.gl/JgMBBcKfTi4pfgKJ6' target='_blank'><i>22710, 22740 Talavera Dr, Laguna Niguel, CA 92677↗</i></a></li><li><a href='https://www.ocparks.com/alisowood' target='_blank'><i>Check for Alerts↗</i></a></li></small></ul>",
+				"headline": "<h6 style='color:red;'>Parking</h6><small style='color:red;'><a href='https://maps.app.goo.gl/JgMBBcKfTi4pfgKJ6' target='_blank'><i>22710, 22740 Talavera Dr, Laguna Niguel, CA 92677↗</i></small>",
 				"text": "Park at the end of Talavera Dr before reaching the gated community. The trail starts at Seaview Park in Laguna Niguel and ends in Laguna Beach at Aliso Peak."
 			}
 			},
@@ -58,7 +58,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Starting Point</h6><small style='color:red;'><ul><li>1.6 miles (Out and Back)</li><li>Very little shade and one moderately steep section</li><li>Duration: 30-45 minutes</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Starting Point</h6><small style='color:red;'><ul><li>1.6 miles (Out and Back)</li><li>Very little shade and one moderately steep section</li><li>Duration: 30-45 minutes</li><li></a></li><li><a href='https://www.ocparks.com/alisowood' target='_blank'><i>Check for Alerts↗</i></a></li></ul></small>",
 				"text": "Start the trail here, at the end of Talavera Drive past Seaview Park on the right side. Enter and go left towards the ocean on the Aliso Peak Trail."
 			}
 			},
