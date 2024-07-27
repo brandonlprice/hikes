eacb019741b924b9bd2e30ee4bc8384f6a53c19d
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-26 18:52:11 -0700

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -13,7 +13,7 @@ let demojs={
 			{
 			"background": {         
                     			"url": "images/IMG_4003_10_11zon.jpg",
-                    			"color": "#121619",
+                    			"color": "",
                     			"opacity": 50
                 	},
 			"date": "Jan 2006",
