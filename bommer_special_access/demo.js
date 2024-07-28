e6d5c5f65eef323adde33134382dceba29048168
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-28 10:38:04 -0700

    Update demo.js

--- bommer_special_access/demo.js
+++ bommer_special_access/demo.js
@@ -11,9 +11,14 @@ let demojs={
 		"map_subdomains":"xyz",
 		"slides": [
 			{
+			"background": {         
+                    		"url": "images/IMG_8918_11zon.jpg",
+                    		"color": "#464646",
+                    		"opacity": 99					
+                	},
 			"date": "Jan 2006",
 			"media": {
-				"url": "images/IMG_8918_11zon.jpg",
+				"url": "",
 				"caption": "<i>Bommer Pass Trail, Bommer Canyon, Irvine, Spring 2022</i>",
 				"credit": ""
 			},
