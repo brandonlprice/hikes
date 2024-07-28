1a35ff4071df439e48d4baea676ca7af970497f4
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-28 09:04:00 -0700

    Update demo.js

--- arboretum/demo.js
+++ arboretum/demo.js
@@ -11,6 +11,11 @@ let demojs={
 		"map_subdomains":"xyz",
 		"slides": [
 			{
+			"background": {         
+                    			"url": "images/24.jpg",
+                    			"color": "#464646",
+                    			"opacity": 99
+                	},
 			"date": "Jan 2006",
 			"media": {
 				"url": "images/24.jpg",
