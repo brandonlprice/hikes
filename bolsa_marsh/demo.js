ce4e0c2ff4090698afefb13b4f7f3076fff28b39
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-28 10:35:48 -0700

    Update demo.js

--- bolsa_marsh/demo.js
+++ bolsa_marsh/demo.js
@@ -11,9 +11,14 @@ let demojs={
 		"map_subdomains":"xyz",
 		"slides": [
 			{
+			"background": {         
+                    		"url": "images/topoftheworld.JPG",
+                    		"color": "#464646",
+                    		"opacity": 99					
+                	},
 			"date": "Jan 2006",
 			"media": {
-				"url": "images/topoftheworld.JPG",
+				"url": "",
 				"caption": "<i></i>",
 				"credit": ""
 			},
