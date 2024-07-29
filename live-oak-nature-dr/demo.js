e7c1086e6dafc1ea092b71eead55b256dd630913
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-29 04:55:13 -0700

    Update demo.js

--- live-oak-nature-dr/demo.js
+++ live-oak-nature-dr/demo.js
@@ -11,10 +11,15 @@ let demojs={
 		"map_subdomains":"xyz",
 		"slides": [
 			{
+			"background": {         
+                    		"url": "images/IMG_7264_11zon.jpg",
+                    		"color": "#292929",
+                    		"opacity": 99					
+                	},	
 			"date": "Jan 2006",
 			"media": {
-				"url": "images/IMG_7264_11zon.jpg",
-				"caption": "<i>Live Oak Cyn Rd, Trabuco Caynon, CA, Summer 2022</i>",
+				"url": "",
+				"caption": "",
 				"credit": ""
 			},
 			"type": "overview",
