2016d7043f5b75a11a5be9e1d66a56b2dc5af346
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-07-29 05:31:51 -0700

    Update demo.js

--- topoftheworld/demo.js
+++ topoftheworld/demo.js
@@ -11,9 +11,14 @@ let demojs={
 		"map_subdomains":"xyz",
 		"slides": [
 			{
+			"background": {         
+                    		"url": "images/IMG_1202_11zon.jpg",
+                    		"color": "#464646",
+                    		"opacity": 99					
+                	},	
 			"date": "Jan 2006",
 			"media": {
-				"url": "images/IMG_1202_11zon.jpg",
+				"url": "",
 				"caption": "<i>Top of the World, Laguna Beach, Fall 2021</i>",
 				"credit": ""
 			},
