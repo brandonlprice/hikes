f5b6e1092c50cdf1ecbd691b35eb82f24f555510
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2024-08-15 16:35:31 -0700

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -73,7 +73,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Starting Point</h6><small style='color:red;'><ul><li>1.6 miles (Out & Back)</li><li>Very little shade, one moderately steep section</li><li>Duration: 30-45 minutes</li></ul></small>",
+				"headline": "<h6 style='color:red;'>Starting Point</h6><small><ul><li>1.6 miles (Out & Back)</li><li>Very little shade, one moderately steep section</li><li>Duration: 30-45 minutes</li></ul></small>",
 				"text": "Start the trail here, at the end of Talavera Drive past Seaview Park on the right side. Enter and go left towards the ocean on the Aliso Peak Trail."
 			}
 			},
