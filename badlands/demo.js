1c5154b6209b2537be7b2a739a45e820e70db470
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-04-05 08:01:49 -0700

    Update demo.js

--- badlands/demo.js
+++ badlands/demo.js
@@ -196,7 +196,7 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "images/IMG_9391_11zon.jpg",
+				"url": "https://www.dropbox.com/scl/fi/i0bm3ewj691f3c8pydz37/badlands.mp4?rlkey=lvg59pkuo7hgte3x938pjlhaq&st=vu0eenod&dl=0",
 				"caption": "",
 				"credit": ""
 			},
