ccc8a120e0851ad555a39a6b9ab5785eee1d071c
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-04-06 08:48:28 -0700

    Update demo.js

--- badlands/demo.js
+++ badlands/demo.js
@@ -280,7 +280,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "Narrow Section",
-				"text": "This area is the most narrow with the steepest slopes, use utmost caution."
+				"text": "This area is the most narrow with the steepest slope, use utmost caution."
 			}
 			},
 			{
