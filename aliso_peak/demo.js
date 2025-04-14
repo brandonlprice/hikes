981e56fec7e27fe5eab7cd05680d1856d58380aa
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-04-14 04:56:53 -0700

    Update demo.js

--- aliso_peak/demo.js
+++ aliso_peak/demo.js
@@ -27,7 +27,7 @@ let demojs={
 				"line": false
 			},
 			"text": {
-				"headline": "<h4 id='first' style='color: red;font-size:1.2em;line-height: .92em;'>Seaview Park to Aliso Peak</h4>",
+				"headline": "Seaview Park to Aliso Peak<small>Laguna Niguel, CA</small>",
 				"text": "The Aliso Peak Trail takes you from Seaview Park to Aliso Peak. At 683 feet, it is one of the closest peaks to the ocean in Orange County. The trail is maintained by OC Parks and is part of Aliso & Wood Canyons Wilderness Park."
 			}
 			},
@@ -50,19 +50,19 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Parking</h6><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3426.1804990546393!2d-117.7327933!3d33.51289990000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcef2187784cc5%3A0xa14170796ded8b93!2sAliso%20Peak%20Trail!5e1!3m2!1sen!2sus!4v1723824569468!5m2!1sen!2sus' width='400' height='300' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe><br><small style='color:red;'><a href='https://maps.app.goo.gl/JgMBBcKfTi4pfgKJ6' target='_blank'><i>22710, 22740 Talavera Dr, Laguna Niguel, CA 92677↗</i></a><br><a href='https://www.ocparks.com/alisowood' target='_blank'><i>Check for Alerts↗</i></a></small>",
+				"headline": "Parking<small><a href='https://maps.app.goo.gl/JgMBBcKfTi4pfgKJ6' target='_blank'>22710, 22740 Talavera Dr, Laguna Niguel, CA 92677↗</a><br><a href='https://www.ocparks.com/alisowood' target='_blank'>Check for Alerts↗</a></small>",
 				"text": "Park at the end of Talavera Dr before reaching the gated community. The trail starts at Seaview Park in Laguna Niguel and ends in Laguna Beach at Aliso Peak."
 			}
 			},
 			{
 			"background": {         
-                    			"url": "images/6296FA42-546F-402D-9D05-94FD9EC76A54_11zon.jpeg",
+                    			"url": "",
                     			"color": "#464646",
                     			"opacity": 99
                 	},
 			"date": "",
 			"media": {
-				"url": "",
+				"url": "images/6296FA42-546F-402D-9D05-94FD9EC76A54_11zon.jpeg",
 				"caption": "",
 				"credit": ""
 			},
@@ -73,19 +73,19 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Starting Point</h6><small><ul><li>1.6 miles (Out & Back)</li><li>Very little shade, one moderately steep section</li><li>Duration: 30-45 minutes</li></ul></small>",
+				"headline": "Starting Point<small><ul><li>1.6 miles (Out & Back)</li><li>Very little shade, one moderately steep section</li><li>Duration: 30-45 minutes</li></ul></small>",
 				"text": "Start the trail here, at the end of Talavera Drive past Seaview Park on the right side. Enter and go left towards the ocean on the Aliso Peak Trail."
 			}
 			},
 			{
 			"background": {         
-                    			"url": "images/IMG_3993_3_11zon.jpg",
+                    			"url": "",
                     			"color": "#464646",
                     			"opacity": 99
                 	},
 			"date": "",
 			"media": {
-				"url": "",
+				"url": "images/IMG_3993_3_11zon.jpg",
 				"caption": "",
 				"credit": ""
 			},
@@ -96,19 +96,19 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Go Straight</h6>",
+				"headline": "Go Straight",
 				"text": "This part is flat and wide with multiple picnic tables located on both sides. People are normally present on this hike and some bring their dogs to this section."
 			}
 			},
 			{
 			"background": {         
-                    			"url": "images/IMG_2771_11zon.jpg",
+                    			"url": "",
                     			"color": "#464646",
                     			"opacity": 99
                 	},
 			"date": "",
 			"media": {
-				"url": "",
+				"url": "images/IMG_2771_11zon.jpg",
 				"caption": "",
 				"credit": ""
 			},
@@ -119,19 +119,19 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Keep Going</h6>",
+				"headline": "Keep Going",
 				"text": "The ocean will begin to appear."
 			}
 			},
 			{
 			"background": {         
-                    			"url": "images/IMG_2776_11zon.jpg",
+                    			"url": "",
                     			"color": "#464646",
                     			"opacity": 99
                 	},	
 			"date": "",
 			"media": {
-				"url": "",
+				"url": "images/IMG_2776_11zon.jpg",
 				"caption": "",
 				"credit": ""
 			},
@@ -142,7 +142,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Go Left</h6>",
+				"headline": "Go Left",
 				"text": "Go left around the bush. A picnic table is on the right but people are usually already there."
 			}
 			},
@@ -160,7 +160,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Clear Ocean Views</h6>",
+				"headline": "Clear Ocean Views",
 				"text": "Enjoy the views before starting a descend."
 			}
 			},
@@ -178,7 +178,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Head Downhill</h6>",
+				"headline": "Head Downhill",
 				"text": "Enter the downhill section between these shrubs. The trail will become very narrow."
 			}
 			},
@@ -196,7 +196,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Downhill</h6>",
+				"headline": "Downhill",
 				"text": " Take your time here. It's steep and rocky."
 			}
 			},
@@ -214,7 +214,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Downhill Continued</h6>",
+				"headline": "Downhill Continued",
 				"text": "Views open up again. Stay alert and watch your footing."
 			}
 			},
@@ -232,7 +232,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Downhill Continued</h6>",
+				"headline": "Downhill Continued",
 				"text": "Another shot on a different day. Trail conditions vary depending when trail maintenance was last done."
 			}
 			},
@@ -250,7 +250,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Enter Residential Area</h6>",
+				"headline": "Enter Residential Area",
 				"text": "The trail breaks left down into a neighborhood with nice houses."
 			}
 			},
@@ -268,7 +268,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Midpoint</h6>",
+				"headline": "Midpoint",
 				"text": "The first half of the hike is over. Follow the arrow to continue."
 			}
 			},
@@ -286,7 +286,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Start Second Half</h6>",
+				"headline": "Start Second Half",
 				"text": "Veer right leaving the residential area towards the peak. There will be another short descend, but not as steep or narrow as the first."
 			}
 			},
@@ -304,7 +304,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Descend</h6>",
+				"headline": "Descend",
 				"text": "The peak can be seen in the distance. The trail is wider and less steep than earlier."
 			}
 			},
@@ -322,7 +322,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Pass Trails</h6>",
+				"headline": "Pass Trails",
 				"text": "The trail is now flat. You will see signs for two offshoot trails. The Valido trail which takes you to Laguna Beach and the Toovet trail a few feet down which goes to a water tank. Go past these."
 			}
 			},
@@ -340,7 +340,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Enter Summit</h6>",
+				"headline": "Enter Summit",
 				"text": "You will be greeted with a flight of stairs once you make it to the base of the peak. Take these to the peak summit."
 			}
 			},
@@ -358,7 +358,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Aliso Peak</h6>",
+				"headline": "Aliso Peak",
 				"text": "Enjoy views from the peak summit. Remember to pace yourself on the way back as the downhills enountered on the way will be uphill!"
 			}
 			} 
