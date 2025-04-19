8c6f4994920f8ad80453d762010093d977a9491f
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-04-19 05:31:34 -0700

    Update demo.js

--- bolsa_marsh/demo.js
+++ bolsa_marsh/demo.js
@@ -42,7 +42,7 @@ let demojs={
 				"lat":  33.695815875358754, 
 				"line": false,
 				"lon": -118.04620311369555,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Starting Point <small><ul><li>2.9 miles (Out & Back)</li><li>Flat, No Shade</li><li>Duration: 1 hr 15 min</li><li>Park at <a href='https://maps.app.goo.gl/VxUbQYgLZext5o2k8' target='_blank'><i>18000 CA-1, Huntington Beach, CA 92648↗</i></a></li></ul></small>",
@@ -60,7 +60,7 @@ let demojs={
 				"lat": 33.69609035969061,   
 				"line": false,
 				"lon": -118.04580346455222,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Starting Out",
@@ -78,7 +78,7 @@ let demojs={
 				"lat": 33.69675845460403, 
 				"line": false,
 				"lon": -118.044709131642,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Left",
@@ -96,7 +96,7 @@ let demojs={
 				"lat": 	33.69781860485279,  
 				"line": false,
 				"lon": -118.04463953175338,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Stay Left",
@@ -114,7 +114,7 @@ let demojs={
 				"lat": 	33.698252927111305,   
 				"line": false,
 				"lon": -118.04468492407936,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Enter Narrow Strip",
@@ -132,7 +132,7 @@ let demojs={
 				"lat": 	33.69888497376734,    
 				"line": false,
 				"lon": -118.04541340547553,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Enjoy the View",
@@ -142,7 +142,7 @@ let demojs={
 			{
 			"date": "",
 			"media": {
-				"url": "https://www.dropbox.com/scl/fi/04vxa65vm9lntunun4txo/bolsa_chica_north.mp4?rlkey=vs0rrgya1plvxgzwdsx88268a&st=9t1vb9vd&raw=1,
+				"url": "https://www.dropbox.com/scl/fi/04vxa65vm9lntunun4txo/bolsa_chica_north.mp4?rlkey=vs0rrgya1plvxgzwdsx88268a&st=9t1vb9vd&raw=1",
 				"caption": "",
 				"credit": ""
 			},
@@ -150,7 +150,7 @@ let demojs={
 				"lat": 	33.69887111983567,     
 				"line": false,
 				"lon": -118.0487362861661,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Enjoy the View",
@@ -168,7 +168,7 @@ let demojs={
 				"lat": 33.70000972942569, 
 				"line": false,
 				"lon": -118.04978749342312,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Straight Away",
@@ -186,7 +186,7 @@ let demojs={
 				"lat": 33.70220240376296,  
 				"line": false,
 				"lon": -118.0509019170528,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Right",
@@ -204,7 +204,7 @@ let demojs={
 				"lat": 33.702628603466486,      
 				"line": false,
 				"lon": -118.05057468756289,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Hard Turn Left",
@@ -222,7 +222,7 @@ let demojs={
 				"lat": 33.70248802474037,  
 				"line": false,
 				"lon": -118.05084022624733,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Viewpoint 1",
@@ -240,7 +240,7 @@ let demojs={
 				"lat": 33.70272901670142,    
 				"line": false,
 				"lon": -118.0509823833208,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Viewpoint 2",
@@ -258,7 +258,7 @@ let demojs={
 				"lat": 33.70270893406381,  
 				"line": false,
 				"lon": -118.05059614523435,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Back Down",
@@ -276,7 +276,7 @@ let demojs={
 				"lat": 33.705438582240575, 
 				"line": false,
 				"lon": -118.04369059063058,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Left",
@@ -294,7 +294,7 @@ let demojs={
 				"lat": 33.70590368663883, 
 				"line": false,
 				"lon": -118.04369581993296,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Left",
@@ -312,7 +312,7 @@ let demojs={
 				"lat": 33.70370701730626, 
 				"line": false,
 				"lon": -118.05266983562521,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Right",
@@ -330,7 +330,7 @@ let demojs={
 				"lat": 33.70404114779711,  
 				"line": false,
 				"lon": -118.05253277038382,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Left",
@@ -348,7 +348,7 @@ let demojs={
 				"lat": 33.70395693133601, 
 				"line": false,
 				"lon": -118.05277444177668,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Turn Left Again",
@@ -366,7 +366,7 @@ let demojs={
 				"lat": 33.703882180340834, 
 				"line": false,
 				"lon": -118.05278114729919,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Take the Stairs",
@@ -384,7 +384,7 @@ let demojs={
 				"lat": 33.70349503607781, 
 				"line": false,
 				"lon": -118.05283881479323,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Concrete Bridge",
@@ -402,7 +402,7 @@ let demojs={
 				"lat": 33.702929001624035, 
 				"line": false,
 				"lon": -118.05158077872896,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Head Back",
