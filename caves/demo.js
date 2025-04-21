ef29d78d2f857a7b0e319fd6512b18026eb889ad
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-04-20 21:43:44 -0700

    Update demo.js

--- caves/demo.js
+++ caves/demo.js
@@ -42,7 +42,7 @@ let demojs={
 				"lat":  33.55178572311904,    
 				"line": false,
 				"lon": -117.71975401347503,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "First Parking Option <br><small><a href='https://maps.app.goo.gl/DkDRVpEBpesLT4vs6' target='_blank'><i>2829 Awma Rd Laguna Niguel, CA 92656↗</i></a></small>",
@@ -60,7 +60,7 @@ let demojs={
 				"lat":  33.551872900440856,     
 				"line": false,
 				"lon": -117.7198425263724,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Alternative Parking<br><small>(Free Option)</small>",
@@ -78,7 +78,7 @@ let demojs={
 				"lat":  33.552002548609146,      
 				"line": false,
 				"lon": -117.72116485541629,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Starting Point<br><small>Aliso Canyon Rd<ul><li>5 miles (Out & Back)</li><li>Flat meadows with no shade</li><li>Duration: 2 hours</li><li><a href='https://www.ocparks.com/alisowood' target='_blank'><i>Check for alerts↗</i></a></li></ul></small>",
@@ -96,7 +96,7 @@ let demojs={
 				"lat":  33.54466783481714,    
 				"line": false,
 				"lon": -117.72721687275848,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Enter Wilderness Park",
@@ -114,7 +114,7 @@ let demojs={
 				"lat":  33.54405256845213,  
 				"line": false,
 				"lon": -117.73184676153136,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Continue on Road",
@@ -132,7 +132,7 @@ let demojs={
 				"lat": 33.54117093876483,  
 				"line": false,
 				"lon": -117.73768324834795,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Wood Canyon Trail",
@@ -150,7 +150,7 @@ let demojs={
 				"lat": 33.543600433917284,
 				"line": false,
 				"lon": -117.7386904178337,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "To Cave Rock",
@@ -168,7 +168,7 @@ let demojs={
 				"lat": 	33.5439312915603,  
 				"line": false,
 				"lon": -117.7396573541832,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Follow the Trail",
@@ -186,7 +186,7 @@ let demojs={
 				"lat": 33.54417384516924, 
 				"line": false,
 				"lon": -117.73978341800675,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Take the Stairs",
@@ -204,7 +204,7 @@ let demojs={
 				"lat": 33.544552746679194,  
 				"line": false,
 				"lon": -117.740256051229,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Cave Rock",
@@ -222,7 +222,7 @@ let demojs={
 				"lat": 33.54430907637847,
 				"line": false,
 				"lon":  -117.7399596671332,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Hike over the top of Cave Rock",
@@ -240,7 +240,7 @@ let demojs={
 				"lat": 33.5449797442232, 
 				"line": false,
 				"lon": -117.74042044264768,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Keep Going",
@@ -258,7 +258,7 @@ let demojs={
 				"lat": 33.545799048592315, 
 				"line": false,
 				"lon": -117.74062965495075,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Downhill",
@@ -276,7 +276,7 @@ let demojs={
 				"lat": 33.54687262735721, 
 				"line": false,
 				"lon": -117.74056796414447,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Keep Going",
@@ -294,7 +294,7 @@ let demojs={
 				"lat": 33.55045450129487, 
 				"line": false,
 				"lon": -117.74145278612535,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "To Dripping Cave",
@@ -312,7 +312,7 @@ let demojs={
 				"lat": 33.55163444215842,  
 				"line": false,
 				"lon": -117.74405728642144,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Cross the Bridge",
@@ -330,7 +330,7 @@ let demojs={
 				"lat": 33.55169591605207, 
 				"line": false,
 				"lon": -117.74420023693143,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
 				"headline": "Dripping Cave",
