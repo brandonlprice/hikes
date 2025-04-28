285e44071a488f8131f6535dfe4d0548e316697a
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-04-28 05:17:23 -0700

    Update demo.js

--- spur-ridge/demo.js
+++ spur-ridge/demo.js
@@ -27,7 +27,7 @@ let demojs={
 				"line": false
 			},
 			"text": {
-				"headline": "<h4 style='color: red;font-size:1.25em;line-height: .92em;'>Spur Ridge Scenic Lookout</h4>",
+				"headline": "Spur Ridge Scenic Lookout<small>Laguna Beach, CA</small>",
 				"text": "This trail is in Laguna Coast Wilderness Park in Laguna Beach. It takes you to a nice lookout of the city and beach below. It is only a little over 2 miles but a small workout is involved as you incline up a hill."
 			}
 			},
@@ -45,7 +45,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Parking</h6> <small style='color:red;'>At the end of <a href='https://maps.app.goo.gl/apAV2ZDUqDbNwuGN8' target='_blank'><i>Dartmoor St, Laguna Beach↗</i></a></small>",
+				"headline": "Parking <small>At the end of <a href='https://maps.app.goo.gl/apAV2ZDUqDbNwuGN8' target='_blank'><i>Dartmoor St, Laguna Beach↗</i></a></small>",
 				"text": "Free residential parking on Dartmoor St. On the weekends, you may have to park further away from the trail entrance if a lot of spots have been taken."
 			}
 			},
@@ -63,7 +63,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Starting Point</h6><small style='color:red;'><ul><li>2.1 miles (Out & Back)</li><li>Uphill climb to a lookout, Ocean Views, No Shade</li></ul></small>",
+				"headline": "Starting Point<small><ul><li>2.1 miles (Out & Back)</li><li>Uphill climb to a lookout, Ocean Views, No Shade</li></ul></small>",
 				"text": "Start the trail here at the end of the street. This area is part of Laguna Coast Wilderness Park."
 			}
 			},
@@ -81,7 +81,7 @@ let demojs={
 				"zoom": 19
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Ocean Views</h6>",
+				"headline": "Ocean Views",
 				"text": "You will immediately be greeted with ocean views as you start climbing up the ridge."
 			}
 			},
@@ -96,10 +96,10 @@ let demojs={
 				"lat": 33.551415864499646, 
 				"line": false,
 				"lon": -117.79487985703275,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Head Uphill</h6>",
+				"headline": "Head Uphill",
 				"text": "Uphill starts before this but now is the focus as it gets steeper and the ocean is behind you."
 			}
 			},
@@ -114,10 +114,10 @@ let demojs={
 				"lat": 33.55292750982752, 
 				"line": false,
 				"lon": -117.79595815170714,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Gradual Incline</h6>",
+				"headline": "Gradual Incline",
 				"text": "Uphill flattens or decreases a little here. A canyon is to the right of the trail."
 			}
 			},
@@ -132,10 +132,10 @@ let demojs={
 				"lat": 33.55579779707898, 
 				"line": false,
 				"lon": -117.7968059199372,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Accelerated Incline</h6>",
+				"headline": "Accelerated Incline",
 				"text": "A slight increase in incline in the last stretch uphill."
 			}
 			},
@@ -150,10 +150,10 @@ let demojs={
 				"lat": 33.557579243703714, 
 				"line": false,
 				"lon": -117.7967683690152,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Turn Left</h6>",
+				"headline": "Turn Left",
 				"text": "Uphill has ended. The trail loops back around going in the other direction."
 			}
 			},
@@ -168,10 +168,10 @@ let demojs={
 				"lat": 33.55688134670505,   
 				"line": false,
 				"lon": -117.7971671335089,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Minor Ascension</h6>",
+				"headline": "Minor Ascension",
 				"text": "A short-lived ascend with partial views of the ocean still."
 			}
 			},
@@ -186,10 +186,10 @@ let demojs={
 				"lat": 33.55504229167387,  
 				"line": false,
 				"lon": -117.7980223017232,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Minor Descension</h6>",
+				"headline": "Minor Descension",
 				"text": "Clear views of the ocean appear again as a brief descend starts."
 			}
 			},
@@ -204,10 +204,10 @@ let demojs={
 				"lat": 33.553978192341376,   
 				"line": false,
 				"lon": -117.7977554733869,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Veering Right</h6>",
+				"headline": "Veering Right",
 				"text": "The trail will naturally veer to the right."
 			}
 			},
@@ -222,10 +222,10 @@ let demojs={
 				"lat": 33.55381714084163,  
 				"line": false,
 				"lon": -117.79842312046107,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Keep Left</h6>",
+				"headline": "Keep Left",
 				"text": "Another path is on the right that leads to a dead end. Stay on the trail farthest to the left."
 			}
 			},
@@ -240,10 +240,10 @@ let demojs={
 				"lat": 33.55375702322952, 
 				"line": false,
 				"lon": -117.79926148228665,
-				"zoom": 19
+				"zoom": 20
 			},
 			"text": {
-				"headline": "<h6 style='color:red;'>Scenic Lookout</h6>",
+				"headline": "Scenic Lookout",
 				"text": "Enjoy the lookout :) When ready, head back the same way you came. There will be nice views on the way out too!"
 			}
 			} 
