3c850f58df6ca6e0814a28e296e13b3834165286
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-05-06 06:24:23 -0700

    Update demo.js

--- barbara_lake/demo.js
+++ barbara_lake/demo.js
@@ -28,7 +28,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "Barbara's Lake<small>Laguna Coast Wilderness Park</small>",
-				"text": "This hike takes you to one of the only three naturally occuring lakes in Orange County. These lakes are all located within a quarter-mile from each other in Laguna Canyon. This one does not dry out and cannot be seen from Laguna Canyon Road."
+				"text": "This hike takes you to one of the only three naturally occuring lakes in Orange County. These lakes are all located within a quarter-mile from each other in Laguna Canyon. This one does not dry out and is hidden from Laguna Canyon Road."
 			}
 			},
 			{
