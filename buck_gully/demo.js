ce16b1c7870aff91658e60c5132a38f15f9d8b17
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-05-08 20:47:01 -0700

    Update demo.js

--- buck_gully/demo.js
+++ buck_gully/demo.js
@@ -28,7 +28,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "Buck Gully Loop",
-				"text": "The Buck Gully trail is in the Newport Coast community of Newport Beach and is managed by the city. The trail is a single-track, but can be hiked in a loop by connecting to other trails like in this example.<ul><li>2.8 mile-loop</li><li>Densely shaded sections, minor inclines, stream crossings</li><li>Park in the <a href='https://maps.app.goo.gl/3dJd9KRpUkzEA8W46' target='_blank'><i>Buck Gully North Parking Lot↗</i></a></li></ul>"
+				"text": "The Buck Gully trail is in the Newport Coast community of Newport Beach and is managed by the city. The trail is a single-track, but can be hiked in a loop by connecting to other trails like in this example.<ul><li>2.8 mile-loop</li><li>Densely shaded sections, minor inclines, stream crossings</li><li>1 hours</li></ul>"
 			}
 			},
 			{
@@ -46,7 +46,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "Start Here",
-				"text": "Start the hike on this sidewalk to the right of the parking lot."
+				"text": "Start the hike on this sidewalk to the right of the parking lot.<br>Park in the <a href='https://maps.app.goo.gl/3dJd9KRpUkzEA8W46' target='_blank'><i>Buck Gully North Parking Lot↗</i></a>"
 			}
 			},
 			{
