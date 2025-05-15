caef733b27daae715c02f4821864f7ab7e78c730
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2025-05-15 02:40:34 -0700

    Update demo.js

--- longview_condensed/demo.js
+++ longview_condensed/demo.js
@@ -28,7 +28,7 @@ let demojs={
 			},
 			"text": {
 				"headline": "Hike to Longview Park<br><font style='line-height: 1em;' size=6>(from Park Paseo St)</font><small>Laguna Niguel, CA</small>",
-				"text": "This is a short hike that starts at a park and goes up a hill to a small summit with ocean views called Longview Park.<ul><li>Half-Mile Round Trip</li><li>A Paved Shaded Short Uphill to a Summit</li></ul>"
+				"text": "This is a short hike that starts at a park and goes up a hill to a small summit with ocean views called Longview Park.<ul><li>Half-Mile Round Trip</li><li>Paved Shaded Short Uphill to a Summit</li></ul>"
 			}
 			},
 			{
